4a4db4d2f72de868cd4887e6bb508dbe929a5cee
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-19 12:22:15 +0100

    🚧 clavier issue

---
+++ src/components/Clavier.vue
@@ -0,0 +1,69 @@
+<script>
+    import { useJournalStore } from '../stores/journal.js'
+
+    export default {
+        name: 'Clavier',
+        props: {
+            contact: {
+                type: Object,
+                required: true
+            }
+        },
+
+        setup() {
+            const user = useJournalStore()
+            return {
+                user
+            }
+        },
+
+        data() {
+            return {
+                phone_number: "",
+            }
+        },
+
+        methods: {
+            addNumber(number) {
+                this.phone_number += number
+            },
+
+            ajoutHistorique(number) {
+                this.user.ajoutHistorique(this.form)
+                this.form = {
+                    id: number,
+                    date: new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDate(),
+                    heure: new Date().getHours() + ":" + new Date().getMinutes(),
+                }
+            },
+        },
+    }
+</script>
+
+
+<template>
+
+    <div class="number">
+        <h1>{{ phone_number }}</h1>
+    </div>
+
+    <div class="existing_contact">
+        <h2></h2>
+    </div>
+
+    <div class="clavier">
+        <button @click="addNumber('1')">1</button>
+        <button @click="addNumber('2')">2</button>
+        <button @click="addNumber('3')">3</button>
+        <button @click="addNumber('4')">4</button>
+        <button @click="addNumber('5')">5</button>
+        <button @click="addNumber('6')">6</button>
+        <button @click="addNumber('7')">7</button>
+        <button @click="addNumber('8')">8</button>
+        <button @click="addNumber('8')">9</button>
+        <button @click="addNumber('0')">0</button>
+    </div>
+
+    <input type="image" src="https://i.pinimg.com/originals/81/45/bd/8145bd48b6b0593d01d69d499d2a1f8f.png" @click="ajoutHistorique(phone_number)">
+
+</template>
--- src/views/ClavierView.vue
+++ src/views/ClavierView.vue
@@ -1,11 +1,24 @@
 <script>
+  import Clavier from '../components/Clavier.vue'
 
+  export default {
+    name: 'ClavierView',
+
+    components: {
+      Clavier,
+    },
+
+  }
 </script>
 
 <template>
   <div class="about">
     <h1>Page du clavier</h1>
   </div>
+
+  <Clavier />
+
+  
 </template>
 
 <style>
