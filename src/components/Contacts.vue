471b349ce5c95d79ee4429d497da60cef38e48fa
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-19 11:16:27 +0100

    contact list view

---
+++ src/components/Contacts.vue
@@ -0,0 +1,22 @@
+<script>
+
+
+    export default {
+
+        name: 'Contacts',
+        props: {
+            contact: {
+                type: Object,
+                required: true
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="contacts-card">
+        <p>{{ contact.name }}</p>
+        <p>{{ contact.number }}</p>
+        <input type="image" src="https://i.pinimg.com/originals/81/45/bd/8145bd48b6b0593d01d69d499d2a1f8f.png">
+    </div>
+</template>
--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,5 +1,27 @@
 <script>
+import { useContactStore } from '../stores/contacts.js'
+import Contacts from '../components/Contacts.vue'
 
+export default {
+  name: 'ContactsView',
+
+  setup() {
+      const user = useContactStore()
+      return {
+          user
+      }
+  },
+
+  components: {
+    Contacts,
+  },
+
+  computed: {
+      contacts() {
+          return this.user.contacts
+      }
+  },
+}
 </script>
 
 
@@ -7,5 +29,27 @@
 <template>
   <div>
     <h1>Page des contacts</h1>
+
+    <div v-for="contact in contacts">
+        <Contacts :contact="contact" />
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+
+    .contacts-card{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 3px solid orange;
+      margin: 10px;
+      padding: 20px;
+      border-radius: 10px;
+    }
+
+    input{
+      width: 25%;
+      height: 25%;
+    }
+</style>
