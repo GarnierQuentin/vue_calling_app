ddad045b032401df4f1a0ebbe992188e51201109
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-19 10:32:19 +0100

    🚚 add contact route update

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,29 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Journal</RouterLink>
-        <RouterLink to="/add_contact">Ajouter un contact</RouterLink>
         <RouterLink to="/clavier">Clavier</RouterLink>
         <RouterLink to="/contacts">contacts</RouterLink>
       </nav>
     </div>
   </header>
 
+  <div id="add">
+    <RouterLink to="/add_contacts"><input type="image" src="https://www.freepnglogos.com/uploads/plus-icon/plus-icon-plus-svg-png-icon-download-1.png"></RouterLink>
+  </div>
   <RouterView />
 </template>
-
+  
 <style scoped>
+  #add{
+    position: fixed;
+    bottom: 10;
+    right: 10;
+    margin: 20px;
+    width: 3%;
+  }
 
+  #add input{
+    width: 100%;
+    height: 100%;
+  }
 </style>
